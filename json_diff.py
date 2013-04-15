d428626807840d7ba506296aa661d4fb5618b121
AJ <info@ajweb.eu>
2013-04-15 19:19:20 +0200

    Redesign and Refactoring

--- json_diff.py
+++ json_diff.py
@@ -1,74 +1,255 @@
-from copy import copy
+import cPickle
+from copy import deepcopy
 
-class AJsonDiff(object):
-    def __init__(self, original, current):
+
+class AJsonDiffBase(object):
+    def __init__(self, current=None, original=None):
         self.original = original
         self.current = current
 
-    def changed(self):
-        return set()
-    def unchanged(self):
-        return set()
+    # an empty diff
     def added(self):
         return set()
+
     def removed(self):
         return set()
 
+    def changed(self):
+        return set()
 
-class AJson(object):
-    _instances = list()
-    _original = _current = _diff = None # for cache
+    def unchanged(self):
+        return set()
 
-    def diff(self):
-        return self._get_diff()
 
-    def _get_diff(self):
-        if not self._cache_valid():
-            return AJsonDiff(self._original, self)
+class AJsonDictDiff(AJsonDiffBase):
+    """
+    A dictionary difference calculator
+    The contents of this class was originally posted here:
+    http://stackoverflow.com/questions/1165352/fast-comparison-between-two-python-dictionary/1165552#1165552
+    """
+
+    def __init__(self, current, original):
+        super(AJsonDictDiff, self).__init__(current, original)
+        self.current_dict, self.past_dict = self.current, self.original
+        self.current_keys, self.past_keys = [
+            set(d.keys()) for d in (self.current, self.original)
+        ]
+        self.intersect = self.current_keys.intersection(self.past_keys)
+
+    def added(self):
+        return set(self.current_keys) - self.intersect
+
+    def removed(self):
+        return set(self.past_keys) - self.intersect
+
+    def changed(self):
+        return set(o for o in self.intersect if self.past_dict[o] != self.current_dict[o])
+
+    def unchanged(self):
+        return set(o for o in self.intersect if self.past_dict[o] == self.current_dict[o])
+
+
+class AJsonListDiff(AJsonDiffBase):
+    """
+    A list difference calculator.
+    Lists may give importance to order, so we return diff like this:
+        - unchanged: Index and Value are the same in both lists
+        - added: Indexes not present in original list but present in current list
+        - removed: Indexes present in original and not in current list
+        - changed: Any changed value in indexes present in both lists.
+                    Note that we DO consider indexes here:
+                    If a value is present in both lists but in different indexes,
+                    it will be caught as changed.
+    """
+
+    def __init__(self, current, original):
+        super(AJsonListDiff, self).__init__(current, original)
+        # we need to make all list entries hashable to convert them to sets
+        self._hashed_current = [cPickle.dumps(o) for o in self.current]
+        self._hashed_original = [cPickle.dumps(o) for o in self.original]
+
+    def added(self):
+        return set(i for i, o in enumerate(self.current)
+                   if i >= len(self.original))
+
+    def removed(self):
+        return set(i for i, o in enumerate(self.original)
+                   if i >= len(self.current))
+
+    def changed(self):
+        """
+            Returns changed items, considering index preservation,
+            i.e.: value present in both lists but on different index
+            will be returned as changed.
+        """
+        changed = []
+        # we'll loop over the smaller list
+        if len(self._hashed_current) < len(self._hashed_original):
+            base_list = self._hashed_current
         else:
-            return self._diff
+            base_list = self._hashed_original
+        for i, o in enumerate(base_list):
+            if self._hashed_original[i] != self._hashed_current[i]:
+                changed.append(i)
+        return set(changed)
 
-    def _cache_valid(self):
-        if self._diff and self._current and self._original == self:
-            return self._diff
+    def unchanged(self):
+        """
+        Both index and value must be the same
+        """
+        unchanged = []
+        # we'll loop over the smaller list
+        if len(self._hashed_current) < len(self._hashed_original):
+            base_list = self._hashed_current
         else:
-            self._current = self
-            self._diff = AJsonDiff(self._original, self)
-            return self._diff
+            base_list = self._hashed_original
+        for i, o in enumerate(base_list):
+            if self._hashed_original[i] == self._hashed_current[i]:
+                unchanged.append(i)
+        return set(unchanged)
 
-    def get_instance(self, obj):
+
+class AJsonLock():
+    LOCKS = ('MODIFICATION', 'ADDITION', 'REMOVAL')
+    _current_locks = set()
+
+    def __init__(self, lock=None):
+        if lock and lock.upper() in self.LOCKS:
+            self._locks.union(set(lock))
+
+    def __unicode__(self):
+        return self.get_current_locks()
+
+    def get_current_locks(self):
+        return self._locks
+
+    def clear_locks(self):
+        self._locks = set()
+
+    def add_lock(self, lock):
+        if lock.upper() in self.LOCKS:
+            self._locks.union(set(lock))
+        else:
+            raise TypeError("The lock %s must be one of %s" % (lock, ",".join(self.LOCKS)))
+
+    def remove_lock(self, lock):
+        if lock.upper() in self.LOCKS:
+            self._locks.discard(lock)
+        else:
+            raise TypeError("The lock %s must be one of %s" % (lock, ",".join(self.LOCKS)))
+
+
+class AJsonBase(object):
+    _meta = {
+        'instances': list(),
+        'original': None,
+        'diff': None,
+        'lock': AJsonLock(),
+        'root': None
+    }
+
+    def __call__(self, obj):
         if isinstance(obj, dict):
-            instance = AJsonDict(obj)
+            instance = AJsonDict(obj, self)
         elif isinstance(obj, list):
-            instance = AJsonList(obj)
-        self._instances.append(instance)
+            instance = AJsonList(obj, self)
+        # self._meta['instances'].append(instance)
+        # self._meta['original'] = deepcopy(instance)
         return instance
 
+    def diff(self):
+        if not self._cache_valid():
+            if isinstance(self._meta['original'], AJsonDict) and isinstance(self, AJsonDict):
+                return AJsonDictDiff(self, self._meta['original'])
+            elif isinstance(self._meta['original'], AJsonList) and isinstance(self, AJsonList):
+                return AJsonListDiff(self, self._meta['original'])
+            else:
+                return AJsonDiffBase()
+        else:
+            print "*** USING CACHE ***"
+            return self._meta['diff']
 
-class AJsonDict(dict, AJson):
-    def __init__(self, adict):
+    def recursive_diff(self):
+        """
+            Gathers the diff objects on every AJson instance
+            and compiles tuples with the changed keys/indexes:
+            >>recursive_diff.change()
+            >>set([(0,'a','c'), (0,'d',0, 'e')])
+        """
+        diffs = []
+        # if no changes
+        if not self._meta['instances']:
+            diffs = set(diffs)
+        else:
+            for i in self._meta['root']._meta['instances']:
+                diffs.append(i.diff())
+        return diffs
+
+    def _cache_valid(self):
+        if self._meta['diff'] and self._meta['original'] == self:
+            return True
+        return False
+
+    def get_instances(self):
+        return self._meta['root']._meta['instances']
+
+
+class AJsonDict(dict, AJsonBase):
+    def __init__(self, adict, root=None):
         super(AJsonDict, self).__init__(adict)
-        self._original = copy(self)
-        self.make_sub_diffs()
+        self._meta['original'] = deepcopy(self)
+        self._meta['root'] = root
+        # self._meta['root']._meta['instances'].append(self)
 
-    def make_sub_diffs(self):
-        for k,v in self.items():
-            if not isinstance(v, basestring)\
-               and not isinstance(v, int)\
-            and not isinstance(v, float):
-                self[k] = self.get_instance(v)
+    def diff(self):
+        return AJsonDictDiff(self, self._meta['original'])
+
+    def __getitem__(self, k):
+        entry = super(AJsonDict, self).__getitem__(k)
+        if isinstance(entry, list) and not isinstance(entry, AJsonList):
+            entry = AJsonList(entry, self._meta['root'])
+            # we are transforming the value, so we need to update it
+            super(AJsonDict, self).__setitem__(k, entry)
+            entry = super(AJsonDict, self).__getitem__(k)
+        elif isinstance(entry, dict) and not isinstance(entry, AJsonDict):
+            entry = AJsonDict(entry, self._meta['root'])
+            # update it
+            super(AJsonDict, self).__setitem__(k, entry)
+            entry = super(AJsonDict, self).__getitem__(k)
+        return entry
+
+    def __setitem__(self, i, y):
+        super(AJsonDict, self).__setitem__(i, y)
+        self._meta['root']._meta['instances'].append(self)
 
 
-class AJsonList(list, AJson):
-    def __init__(self, alist):
+class AJsonList(list, AJsonBase):
+    def __init__(self, alist, root=None):
         super(AJsonList, self).__init__(alist)
-        self._original = copy(self)
-        self.make_sub_diffs()
+        self._meta['original'] = deepcopy(self)
+        self._meta['root'] = root
+        # print self._meta['root']._meta['instances']
 
-    def make_sub_diffs(self):
-        for i,v in enumerate(self):
-            if not isinstance(v, basestring)\
-               and not isinstance(v, int)\
-            and not isinstance(v, float):
-                self[i] = self.get_instance(v)
+    def diff(self):
+        return AJsonListDiff(self, self._meta['original'])
 
+    def __getitem__(self, i):
+        entry = super(AJsonList, self).__getitem__(i)
+        if isinstance(entry, list) and not isinstance(entry, AJsonList):
+            entry = AJsonList(entry, self._meta['root'])
+            # we are transforming the value, so we need to update it
+            super(AJsonList, self).__setitem__(i, entry)
+            entry = super(AJsonList, self).__getitem__(i)
+        elif isinstance(entry, dict) and not isinstance(entry, AJsonDict):
+            entry = AJsonDict(entry, self._meta['root'])
+            # update it
+            super(AJsonList, self).__setitem__(i, entry)
+            entry = super(AJsonList, self).__getitem__(i)
+        return entry
+
+    def __setitem__(self, key, value):
+        super(AJsonList, self).__setitem__(key, value)
+        self._meta['root']._meta['instances'].append(self)
+
+
+SuperJson = AJsonBase
--- tests.py
+++ tests.py
@@ -1,63 +1,169 @@
 from unittest import TestCase
 import simplejson
-from json_diff import AJson, AJsonDiff
+from json_diff import AJsonBase, AJsonDiffBase, AJsonDictDiff, AJsonListDiff, AJsonDict, AJsonList, SuperJson
+
+
+JSON_STRING_SAMPLE = '[{"a": {"b": 1, "c": "t"}, "d": [{"e": "u", "f": 2},{"e": "v", "f": 3}], "g": {"12":"abcd"}}]'
+
 
 class ImplementationTests(TestCase):
 
     def setUp(self):
-        self.json_str='[{"a": {"b": 1, "c": "t"}, "d": [{"e": "u", "f": 2},{"e": "v", "f": 3}]}]'
-        self.json = simplejson.loads(self.json_str)
-        self.ajson = AJson().get_instance(self.json)
+        json = simplejson.loads(JSON_STRING_SAMPLE)
+        superj = AJsonBase()
+        self.ajson = superj(json)
         self.counter = 0
 
+    def tearDown(self):
+        self.superj = None
+        self.json = None
+        self.ajson = None
+
     def test_create_json_diff(self):
-        self.assertTrue( isinstance(self.ajson, AJson) )
+        self.assertTrue(isinstance(self.ajson, AJsonBase))
 
-    def test_iterate_as_dict(self):
-        for i in self.ajson:
+    def test_getting_entry_returns_correct_json_obj(self):
+        entry1 = self.ajson[0]
+        self.assertTrue(isinstance(self.ajson, AJsonList))
+        self.assertTrue(isinstance(entry1, AJsonDict))
+        entry2 = entry1['a']
+        self.assertTrue(isinstance(entry2, AJsonDict))
+        entry3 = entry1['d']
+        entry4 = entry3[0]
+        self.assertTrue(isinstance(entry4, AJsonDict))
+
+    def test_iterate_list(self):
+        for i, entry in enumerate(self.ajson):
+            self.assertTrue(entry, self.ajson[i])
             self.counter += 1
-        self.assertEquals( len(self.ajson), self.counter )
+        self.assertEquals(len(self.ajson), self.counter)
 
-    def test_sub_objects_are_diffs(self, diff_obj=None):
-        if not diff_obj:
-            diff_obj = self.ajson
-        for i,v in enumerate(diff_obj):
-            if not isinstance(v, basestring) and not isinstance(v, int) and not isinstance(v, float):
-                self.assertTrue( isinstance(v, AJson) )
-                self.test_sub_objects_are_diffs(v)
-
-    def test_list_objects_are_diffs(self):
-        obj = '[{"a":[1,2,3]}]'
-        obj_json = simplejson.loads(obj)
-        ajson = AJson().get_instance(obj_json)
-        self.assertTrue( isinstance(ajson[0]['a'], AJson) )
+    def test_iterate_dict(self):
+        for k, v in self.ajson[0].items():
+            self.assertEquals(v, self.ajson[0][k])
+            self.counter += 1
+        self.assertEquals(len(self.ajson[0]), self.counter)
 
     def test_keep_original(self):
-        self.ajson[0]['a']['b'] = 0
-        self.assertEquals( self.ajson._original[0]['a']['b'], 1 )
-        self.assertEquals( self.ajson[0]['a']['b'], 0 )
+        self.ajson[0]['a']['b'] = 2
+        self.assertEqual(1, self.ajson[0]['a']._meta['original']['b'])
+
+    def test_assignment_on_dict(self):
+        c = self.ajson[0]['a']
+        c['b'] = 123
+        self.assertEquals(123, c['b'])
+        self.ajson[0]['a']['b'] = 456
+        self.assertEquals(456, self.ajson[0]['a']['b'])
 
     def test_get_all_instances(self):
-        for i in self.ajson._instances:
-            self.assertTrue( isinstance(i, AJson) )
-            self.assertTrue( isinstance(i.diff(), AJsonDiff) )
-            self.assertTrue( isinstance(i.diff().changed(), set) )
+        print "instances at test_get_all_instances: ", self.ajson.get_instances()
+        # print self.ajson[0]['a']
+        for i in self.ajson.get_instances():
+            self.assertTrue(isinstance(i, AJsonBase))
+            self.assertTrue(isinstance(i.diff().changed(), set))
 
+    def test_list_return_list_diff(self):
+        o = self.ajson[0]['d']
+        diff = o.diff()
+        self.assertTrue(isinstance(diff, AJsonListDiff))
+
+    def test_dict_return_dict_diff(self):
+        obj = self.ajson[0]
+        self.assertTrue(isinstance(obj, AJsonDict))
+        diff = obj.diff()
+        self.assertTrue(isinstance(diff, AJsonDictDiff))
 
 
 class BasicFunctionalityTests(TestCase):
     def setUp(self):
-        self.json_str='[{"a": {"b": 1, "c": "t"}, "d": [{"e": "u", "f": 2},{"e": "v", "f": 3}]}]'
-        self.json = simplejson.loads(self.json_str)
-        self.ajson = AJson().get_instance(self.json)
+        self.json = simplejson.loads(JSON_STRING_SAMPLE)
+        self.superj = SuperJson()
+        self.ajson = self.superj(self.json)
 
     def test_get_diff_instance(self):
-        self.assertTrue( isinstance(self.ajson.diff(), AJsonDiff) )
+        self.assertTrue(isinstance(self.ajson.diff(), AJsonDiffBase))
 
-    def test_getting_changes_return_sets(self):
-        diff = self.ajson.diff()
-        self.assertTrue( isinstance(diff.changed(), set) )
-        self.assertTrue( isinstance(diff.unchanged(), set) )
-        self.assertTrue( isinstance(diff.removed(), set) )
-        self.assertTrue( isinstance(diff.added(), set) )
+    def test_getting_changes_from_list_diffs(self):
+        diff = self.ajson[0]['d'].diff()
+        self.assertTrue(isinstance(diff.changed(), set))
+        self.assertTrue(isinstance(diff.unchanged(), set))
+        self.assertTrue(isinstance(diff.removed(), set))
+        self.assertTrue(isinstance(diff.added(), set))
 
+    def test_getting_changes_from_dict_diffs(self):
+        diff = self.ajson[0].diff()
+        self.assertTrue(isinstance(diff.changed(), set))
+        self.assertTrue(isinstance(diff.unchanged(), set))
+        self.assertTrue(isinstance(diff.removed(), set))
+        self.assertTrue(isinstance(diff.added(), set))
+
+    def test_diff_for_changed_value_in_dict(self):
+        # a change in nested dict
+        self.ajson[0]['a']['b'] = 'c'
+        diff = self.ajson[0]['a'].diff()
+        self.assertEquals(diff.changed(), set('b'))
+        # a change in a dict nested in a list
+        self.ajson[0]['d'][0]['e'] = 12
+        diff = self.ajson[0]['d'][0].diff()
+        self.assertEquals(diff.changed(), set('e'))
+
+    # ******  lists
+    # TODO: put in separate class
+    # remainder: json_str = '[{"a": {"b": 1, "c": "t"}, "d": [{"e": "u", "f": 2}, {"e": "v", "f": 3}]}]'
+    def test_diff_for_changed_value_in_list(self):
+        self.ajson[0]['d'][0] = 12
+        diff = self.ajson[0]['d'].diff()
+        self.assertEquals(diff.changed(), set([0]))
+        self.assertEquals(diff.added(), set([]))
+        self.assertEquals(diff.removed(), set([]))
+        self.assertEquals(diff.unchanged(), set([1]))
+
+        # now change another object
+        self.ajson[0]['a']['b'] = {"e": "v", "f": 1}
+        diff = self.ajson[0]['a'].diff()
+        self.assertEquals(diff.unchanged(), set(['c']))
+        self.assertEquals(diff.changed(), set(['b']))
+        self.assertEquals(diff.added(), set([]))
+        self.assertEquals(diff.removed(), set([]))
+
+    def test_diff_for_added_entry_in_list(self):
+        self.ajson[0]['d'].append(12)
+        diff = self.ajson[0]['d'].diff()
+        self.assertEquals(diff.added(), set([2]))
+        self.assertEquals(diff.removed(), set([]))
+        self.assertEquals(diff.changed(), set([]))
+        self.assertEquals(diff.unchanged(), set([0, 1]))
+
+    def test_diff_for_removed_entry_in_list(self):
+        #note: of course, list are re-ordered when removing entries
+        del self.ajson[0]['d'][0]
+        diff = self.ajson[0]['d'].diff()
+        self.assertEquals(diff.removed(), set([1]))
+        self.assertEquals(diff.added(), set([]))
+        self.assertEquals(diff.changed(), set([0]))
+        self.assertEquals(diff.unchanged(), set([]))
+
+
+class NestedDiffsTests(TestCase):
+    # remainder: JSON_STRING_SAMPLE = '[{"a": {"b": 1, "c": "t"}, "d": [{"e": "u", "f": 2},{"e": "v", "f": 3}]}, "g": {12:"abcd"}]'
+
+    def setUp(self):
+        self.json = simplejson.loads(JSON_STRING_SAMPLE)
+        self.superj = SuperJson()
+        self.ajson = self.superj(self.json)
+
+    def test_get_global_diff(self):
+        self.ajson[0]['d'][0]['e'] = 'si sisi'
+        self.ajson[0]['a']['c'] = 'nonono'
+        print self.ajson[0]['d'][0].diff().changed()
+        self.ajson[0]['a']['c'] = 'nonono'
+        print self.ajson[0]['d'][0].diff().changed()
+        print self.ajson[0]['a'].diff().changed()
+        self.ajson[0]['g']["12"] = "yes"
+        print self.ajson[0]['g'].diff().changed()
+        diffs = self.ajson.recursive_diff()
+
+        self.assertEquals(
+            diffs.changed(),
+            set([(0, 'a', 'c'), (0, 'd', 0, 'e')])
+        )
